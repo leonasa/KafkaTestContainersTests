aac9f5cd5538595c75e9faa65061669d78147cf7
Leonardo Santana <leonardonsantana@gmail.com>
2023-01-13 17:38:37 -0500

    using mapped port:  kafkacat -L -b 127.0.0.1:49304
     kafkacat -L -b 127.0.0.1:49304

--- KafkaTestContainersTests/KafkaTestContainerTest.cs
+++ KafkaTestContainersTests/KafkaTestContainerTest.cs
@@ -53,12 +53,14 @@ public class KafkaTestContainerTest
         await network.CreateAsync();
         _testOutputHelper.WriteLine("Network started");
 
-        var kafkaPort = FreeTcpPort();
+        var kafkaPort = 9092;
+        // var kafkaPort = FreeTcpPort();
         var kafkaTestcontainerConfiguration = new KafkaTestcontainerConfigurationNew(kafkaPort);
         
         var kafka = new TestcontainersBuilder<KafkaTestcontainer>()
             .WithNetwork(network)
-            .WithPortBinding(kafkaPort, 9092)
+            .WithPortBinding(9092)
+            // .WithPortBinding(9092, true)
             .WithName("broker")
             .WithNetworkAliases("broker")
             .WithKafka(kafkaTestcontainerConfiguration)
@@ -96,13 +98,14 @@ public class KafkaTestContainerTest
         ProducerConfig producerConfig = new ProducerConfig
         {
             BootstrapServers = kafka.BootstrapServers
+            // BootstrapServers = "localhost:9092"
         };
         
         using var producer = new ProducerBuilder<string, User>(producerConfig)
             .SetValueSerializer(new JsonSerializer<User>(schemaRegistry).AsSyncOverAsync())
             .Build();
 
-        _testOutputHelper.WriteLine("producer built");
+        _testOutputHelper.WriteLine($"producer built with config {producerConfig.BootstrapServers}");
         
         var deliveryResult = await producer.ProduceAsync("test", new Message<string, User>());
         producer.Flush(TimeSpan.FromSeconds(1));
@@ -116,27 +119,40 @@ public record User(string name, int id);
 
 public class KafkaTestcontainerConfigurationNew : KafkaTestcontainerConfiguration
 {
+    private readonly int _kafkaPort;
+    private static ushort _mappedPublicPort;
     private const int KafkaPort = 9092;
     private const int BrokerPort = 29092;
     private const int ZookeeperPort = 2181;
 
     private const string StartupScriptPath = "/testcontainers_start.sh";
 
+    public override string[] Command { get; }
+        = { "/bin/sh", "-c", $"echo 'running script tata'; while [ ! -f {StartupScriptPath} ]; do sleep 0.1; echo 'waiting script file'; done; . {StartupScriptPath}; echo 'end script tata';" };
     
     public KafkaTestcontainerConfigurationNew(int kafkaPort)
+        : base("confluentinc/cp-kafka:latest")
     {
+        _kafkaPort = kafkaPort;
         Environments["KAFKA_LISTENER_SECURITY_PROTOCOL_MAP"] = "PLAINTEXT:PLAINTEXT,PLAINTEXT_HOST:PLAINTEXT";
-        Environments["KAFKA_ADVERTISED_LISTENERS"] = $"PLAINTEXT://broker:29092,PLAINTEXT_HOST://localhost:{kafkaPort}";
+        // Environments["KAFKA_ADVERTISED_LISTENERS"] = $"PLAINTEXT://broker:29092,PLAINTEXT_HOST://localhost:{kafkaPort}";
         Environments["KAFKA_INTER_BROKER_LISTENER_NAME"] = "PLAINTEXT";
         Environments.Remove("KAFKA_LISTENERS");
     }
+    
+    public override IWaitForContainerOS WaitStrategy => Wait.ForUnixContainer()
+        .UntilPortIsAvailable(_mappedPublicPort);
 
     public override Func<IRunningDockerContainer, CancellationToken, Task> StartupCallback
         => (container, ct) =>
         {
+            _mappedPublicPort = container.GetMappedPublicPort(9092);
+
             const char lf = '\n';
             var startupScript = new StringBuilder();
             startupScript.Append("#!/bin/sh");
+            startupScript.Append(lf);
+            startupScript.Append("echo 'inside script'");
             startupScript.Append(lf);
             startupScript.Append($"echo 'clientPort={ZookeeperPort}' > zookeeper.properties");
             startupScript.Append(lf);
@@ -146,11 +162,11 @@ public class KafkaTestcontainerConfigurationNew : KafkaTestcontainerConfiguratio
             startupScript.Append(lf);
             startupScript.Append("zookeeper-server-start zookeeper.properties &");
             startupScript.Append(lf);
+            startupScript.Append($"export KAFKA_ADVERTISED_LISTENERS='PLAINTEXT://broker:29092,PLAINTEXT_HOST://localhost:9092'");
+            startupScript.Append(lf);
             startupScript.Append(". /etc/confluent/docker/bash-config");
             startupScript.Append(lf);
             startupScript.Append("/etc/confluent/docker/run");
-            return container.CopyFileAsync(StartupScriptPath, Encoding.Default.GetBytes(startupScript.ToString()),
-                0x1ff, ct: ct);
-           
+            return container.CopyFileAsync(StartupScriptPath, Encoding.Default.GetBytes(startupScript.ToString()), 0x1ff, ct: ct);
         };
 }
